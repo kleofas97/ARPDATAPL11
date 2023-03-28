2628363372a8e0a0ff4ac57829661017245ed0c6
kleofas971 <mika.adam@outlook.com>
2023-03-28 20:48:56 +0200

    dodatkowe zadania + dodanie gitignore

--- ARPDATAPL11_07/ex_02_wywolanie.py
+++ ARPDATAPL11_07/ex_02_wywolanie.py
@@ -3,3 +3,4 @@
 #  TODO Dodaj import kalkulatora
 # calc = ex_02_calculator.Calculator(5,6)
 # print(calc.add())  # wypisz 11
+# TODO dodaj druga instancje kalkulatora i zobacz czy licznik operacji dobrze dziala
