7a0c04ed60b979868f7dec680267078afb9f3831
kleofas971 <mika.adam@outlook.com>
2023-03-29 20:50:58 +0200

    Rectangle tests

--- ARPDATAPL11_08/ex_01_Rectangle_get_set.py
+++ ARPDATAPL11_08/ex_01_Rectangle_get_set.py
@@ -3,9 +3,11 @@
 # metody get area oraz get diagonal
 import math
 
+
 class Rectangle:
 
     def __init__(self, bok1, bok2):
+        # TODO - a jak chronic dane przy przypisaniu? podpowiedz: https://realpython.com/python-getter-setter/
         self._bok1 = bok1
         self._bok2 = bok2
 
@@ -14,33 +16,35 @@ class Rectangle:
         return self._bok1
 
     @bok1.setter
-    def bok1(self,new_bok):
-        if isinstance(new_bok, (int,float)):
+    def bok1(self, new_bok):
+        if isinstance(new_bok, (int, float))  and new_bok > 0:
             self._bok1 = new_bok
         else:
             print('Zla wartosc - bok1 niezmieniony')
 
     @property
     def bok2(self):
-        return self._bok1
+        return self._bok2
 
     @bok2.setter
     def bok2(self, new_bok):
-        if isinstance(new_bok, (int, float)):
+        if isinstance(new_bok, (int, float)) and new_bok > 0:
             self._bok2 = new_bok
         else:
-            print('Zla wartosc - bok1 niezmieniony')
+            print('Zla wartosc - bok2 niezmieniony')
 
     def get_area(self):
         return self._bok1 * self._bok2
 
     def get_diagonal(self):
-        return math.sqrt(self._bok1**2 + self._bok2**2)
+        return math.sqrt(self._bok1 ** 2 + self._bok2 ** 2)
 
-rec = Rectangle(5,4)
-print(rec.bok1)
-rec.bok1 = 3
-rec.bok1 = 'tekst'
-rec.bok1 = [234]
-print(rec.bok1)
-print(rec.get_diagonal())
+
+if __name__ == "__main__":
+    rec = Rectangle(5, 4)
+    print(rec.bok1)
+    rec.bok1 = 3
+    rec.bok1 = 'tekst'
+    rec.bok1 = [234]
+    print(rec.bok1)
+    print(rec.get_diagonal())
--- ARPDATAPL11_08/test_ex_01_Rectangle_get_set.py
+++ ARPDATAPL11_08/test_ex_01_Rectangle_get_set.py
@@ -1,17 +1,51 @@
 from unittest import TestCase
+from ARPDATAPL11_08.ex_01_Rectangle_get_set import Rectangle
+import numpy as np
 
 
 class TestRectangle(TestCase):
 
+    def setUp(self) -> None:
+        self.rec = Rectangle(5, 5)
 
     def test_bok1(self):
-        self.fail()
+        expected = 10
+        self.rec.bok1 = expected
+        assert self.rec.bok1 == expected
+        not_expected = 'tekst'
+        self.rec.bok1 = not_expected
+        assert self.rec.bok1 != not_expected
 
     def test_bok2(self):
-        self.fail()
+        expected = 10
+        self.rec.bok1 = expected
+        assert self.rec.bok1 == expected
+        not_expected = 'tekst'
+        self.rec.bok2 = not_expected
+        assert self.rec.bok2 != not_expected
 
     def test_get_area(self):
-        self.fail()
+        self.rec.bok1 = 10
+        self.rec.bok2 = 10
+        assert self.rec.get_area() == 100
 
     def test_get_diagonal(self):
-        self.fail()
+        self.rec.bok1 = 3
+        self.rec.bok2 = 4
+        assert self.rec.get_diagonal() == 5.0
+
+    def test_get_diagonal_wymierna(self):
+        self.rec.bok1 = 10
+        self.rec.bok2 = 10
+        wynik = np.round(self.rec.get_diagonal(), 2)
+        assert wynik == np.round(np.sqrt(200), 2)
+
+    def test_negative_numbers_bok1(self):
+        not_expected = -10
+        self.rec.bok1 = not_expected
+        assert self.rec.bok1 != not_expected
+
+    def test_negative_numbers_bok2(self):
+        not_expected = -10
+        self.rec.bok2 = not_expected
+        assert self.rec.bok2 != not_expected
