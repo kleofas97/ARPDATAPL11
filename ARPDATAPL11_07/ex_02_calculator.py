7535f2eaf3adcc7643014254c5e2c451ef644d49
kleofas971 <mika.adam@outlook.com>
2023-03-29 17:55:42 +0200

    rozwiazanie zadanie domowe z lekcji 7

--- ARPDATAPL11_07/ex_02_calculator.py
+++ ARPDATAPL11_07/ex_02_calculator.py
@@ -1,4 +1,3 @@
-# TODO zadanie
 # napisz klase Calculator
 # 2 atrybuty ( okreslane przy inicjacji) a i b
 # 4 metody do obliczen - (ktore zwracaja wynik odpowiednio mnozenia, dzielenia, dodawania i odejmowania)
@@ -6,3 +5,45 @@
 # upgrade - 1 - dodaj liczenie ile operacji juz bylo
 #     - w danym kalkulatorze
 # - we wszystkich kalkulatorach
+
+class Calculator:
+    number_of_operation_in_all_cals = 0
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.number_of_operation = 0
+
+    def get_sum(self):
+        self.add_counters()
+        return self.a + self.b
+
+    def get_substract(self):
+        self.add_counters()
+        return self.a - self.b
+
+    def get_multiply(self):
+        self.add_counters()
+        return self.a * self.b
+
+    def def_divide(self):
+        self.add_counters()
+        return self.a / self.b
+
+    def add_counters(self):
+        self.number_of_operation += 1
+        Calculator.number_of_operation_in_all_cals += 1
+
+
+if __name__ == "__main__":
+    calc = Calculator(5, 6)
+    calc.get_sum()
+    calc.get_substract()
+    calc.get_multiply()
+    calc2 = Calculator(4, 3)
+    calc2.get_sum()
+    print(f'Liczba operacji w calc1: {calc.number_of_operation}')
+    print(f'Liczba operacji w calc2: {calc2.number_of_operation}')
+    print(
+        f'Liczba operacji we wszystkich kalkulatorach: '
+        f'{Calculator.number_of_operation_in_all_cals}')
--- ARPDATAPL11_07/ex_02_wywolanie.py
+++ ARPDATAPL11_07/ex_02_wywolanie.py
@@ -1,6 +1,13 @@
-# import ....
+import ARPDATAPL11_07.ex_02_calculator as calculator
 
-#  TODO Dodaj import kalkulatora
-# calc = ex_02_calculator.Calculator(5,6)
-# print(calc.add())  # wypisz 11
-# TODO dodaj druga instancje kalkulatora i zobacz czy licznik operacji dobrze dziala
+calc = calculator.Calculator(5, 6)
+calc.get_sum()
+calc.get_substract()
+calc.get_multiply()
+calc2 = calculator.Calculator(4, 3)
+calc2.get_sum()
+print(f'Liczba operacji w calc1: {calc.number_of_operation}')
+print(f'Liczba operacji w calc2: {calc2.number_of_operation}')
+print(
+    f'Liczba operacji we wszystkich kalkulatorach: '
+    f'{calculator.Calculator.number_of_operation_in_all_cals}')
