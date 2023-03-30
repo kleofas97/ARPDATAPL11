43dd482f37b863df4bbef8321f9f5ef5a900f802
kleofas971 <mika.adam@outlook.com>
2023-03-30 18:33:09 +0200

    rozwiazanie odd indexes

--- ARPDATAPL11_09/ex_00_odd_indexes.py
+++ ARPDATAPL11_09/ex_00_odd_indexes.py
@@ -1,6 +1,14 @@
-# napisz funkcje ktora njapierw sprawdzi czy input jest stringiem
+# napisz funkcje ktora najpierw sprawdzi czy input jest stringiem
 # a potem zwroci co druga litere, zaczynajac od 2 litery
 
 
-def odd_indexes(tekst: str) -> str:
-    pass
+def odd_indexes(tekst: str):
+    if isinstance(tekst, str):
+        return tekst[1::2]
+    return None  # ewentualnie '' (pusty string)
+
+
+if __name__ == "__main__":
+    print(odd_indexes('Tester'))
+    # 'Teleturniej' -> 'eeune'
+    # 'Tekst' -> 'es'
