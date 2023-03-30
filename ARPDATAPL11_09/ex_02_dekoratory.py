6b8a8768c773003a04c44d05dd21aab2a93472e4
kleofas971 <mika.adam@outlook.com>
2023-03-30 18:54:57 +0200

    przyklad dekoratorow

--- ARPDATAPL11_09/ex_02_dekoratory.py
+++ ARPDATAPL11_09/ex_02_dekoratory.py
@@ -1,13 +1,26 @@
 import time
 
 
+def timer(func):
+    def wrap_function(*args, **kwargs):
+        start_time = time.time()
+        print('Rozpoczeto mierzenie czasu - funkcja dopiero sie zacznie')
+        result = func(*args, **kwargs)
+        print('Zakonczylem funkcje')
+        end_time = time.time()
+        print(f'Function {func.__name__} executed in {end_time - start_time}')
+        return result
+
+    return wrap_function
 
 
+@timer
 def my_long_function(n):
     variable = 0
     print("jestem w funkcji")
     for i in range(100000 * n):
         variable += variable + i
+    return None
 
 
 my_long_function(2)
